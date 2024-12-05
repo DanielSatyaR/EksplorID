29e7c7def1085dba97d91962e8490b2be047a3ab
RamaRefin <Rama.223040040@mail.unpas.ac.id>
2024-12-05 20:54:53 +0700

    Progress Homepage 1.4

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -71,7 +71,7 @@
                 <h3 class="mt-1 text-lg font-bold text-red-500">Rp.27.000</h3>
             </a>
                 </div>
-            </div>
+             </div>
         </div>
     </div>
 </div>
@@ -85,26 +85,52 @@
         <a href="#" class="text-1xl text-right mb-6 block">Lainnya &gt;&gt;</a>
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <!-- Card 1 -->
-            <a href="link-anda.html" class="block">
+            <a href="#" class="block">
             <div class="relative w-full h-48 rounded-xl overflow-hidden shadow-md">
-                <img src="img/bali.jpeg" alt="Jakarta" class="w-full h-full object-cover">
+                <img src="img/Bali.jpeg" alt="Jakarta" class="w-full h-full object-cover">
                 <div class="absolute bottom-2 left-2 bg-opacity-50 text-white text-lg font-semibold px-3 py-1 rounded">Bali</div>
             </div>
         </a>
             <!-- Card 2 -->
-        <a href="link-anda.html" class="block">
+        <a href="#" class="block">
             <div class="relative w-full h-48 rounded-xl overflow-hidden shadow-md">
-                <img src="img/jakarta.jpg" alt="Jakarta" class="w-full h-full object-cover">
+                <img src="img/Jakarta.jpg" alt="Jakarta" class="w-full h-full object-cover">
                 <div class="absolute bottom-2 left-2 bg-opacity-50 text-white text-lg font-semibold px-3 py-1 rounded">Jakarta</div>
             </div>
         </a>
 
             <!-- Card 3 -->
-            <a href="link-anda.html" class="block">
+            <a href="#" class="block">
             <div class="relative w-full h-48 rounded-xl overflow-hidden shadow-md">
-                <img src="img/yogya.png" alt="Jakarta" class="w-full h-full object-cover">
+                <img src="img/Yogya.png" alt="Jakarta" class="w-full h-full object-cover">
                 <div class="absolute bottom-2 left-2 bg-opacity-50 text-white text-lg font-semibold px-3 py-1 rounded">Yogyakarta</div>
             </div>
         </a>
+    </div>
+</div>
+<!----------------------------------------------KONTEN 3------------------------------------------------------>
+
+<div class="Konten2">
+    <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold text-left mb-6">Temukan Liburan di kota lainnya</h1>
+<div class="card-container">
+    <div class="card">
+      <img src="img/shield.png" alt="Pembayaran Aman" class="card-icon">
+      <h2>Pembayaran aman dan nyaman</h2>
+      <p> Nikmati transaksi dengan keamanan berlapis, dengan opsi pembayaran yang lebih dari satu</p>
+    </div>
+    <div class="card">
+      <img src="img/order.png" alt="Pemesanan Fleksibel" class="card-icon">
+      <h2>Pemesanan cepat & fleksibel</h2>
+      <p> Pesan tiket online dalam hitungan menit, dengan pembatalan gratis untuk banyak aktivitas</p>
+    </div>
+    <div class="card">
+      <img src="img/xperience.png" alt="Pengalaman Seru" class="card-icon">
+      <h2>Pengalaman seru</h2>
+      <p> Pengalaman liburan yang sangat menyenangkan, dan tidak akan pernah bosan</p>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
