63fd7ae520f9873b2cb2303db4d275e1fc7ebe1e
Eggy Bagus <eggybagushidayatullah@gmail.com>
2024-12-15 17:34:48 +0700

    Memperbaiki Login Regis

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,10 +17,25 @@
             </ul>
         </nav>
         <div class="auth-buttons">
-            <a href="#" class="sign-in">Sign in</a>
+            <a href="/login" class="sign-in">Sign in</a>
             <a href="/createacc" class="create-account">Create Account</a>
         </div>
     </header>
+
+    @if(session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    @if(session()->has('loginError'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('loginError') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
     <div class="container">
         <div class="image-container">
             <img src="img/sea.jpg" alt="Sea">
