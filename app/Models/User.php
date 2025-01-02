a1f21f6e615636ba77c9d6cb2b03733f5dbc0384
Fikryawan.n <85277732+fikryawan7@users.noreply.github.com>
2025-01-02 23:25:57 +0700

    add user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -17,14 +15,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'noHp',
-    //     'password',
-    // ];
-
-    protected $guarded = ['id'];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role', // Tambahkan kolom role di sini
+    ];
 
     /**
      * The attributes that should be hidden for serialization.
